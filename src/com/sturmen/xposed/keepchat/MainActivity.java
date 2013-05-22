151cd1215cc821981554342db13efc91c6ec1da6
Nick Tinsley <00sturm@gmail.com>
2013-05-22 02:58:56 -0400

    More cleanup from menu removal.

--- src/com/sturmen/xposed/keepchat/MainActivity.java
+++ src/com/sturmen/xposed/keepchat/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
